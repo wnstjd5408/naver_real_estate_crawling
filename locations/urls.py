3e79f101a3dd6f55d04422e88d99d28ce96caa76
paka_coding <wnstjd5408@naver.com>
2022-06-03 09:47:46 +0900

    feat: URL 추가

--- locations/urls.py
+++ locations/urls.py
@@ -4,6 +4,4 @@ from .views import *
 
 app_name = "location"
 
-urlpatterns = [
-    path("", LocationListView.as_view(), name="idx"),
-]
+urlpatterns = [path("", LocationListView.as_view(), name="idx"), path("<int:pk>", LocationDetailView.as_view(), name="detail")]
