dc428b4469398615e63df3467f15d1c6b893841a
paka_coding <wnstjd5408@naver.com>
2022-06-07 13:14:03 +0900

    feat: admin 추가

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Basket
+
 # Register your models here.
+
+
+@admin.register(Basket)
+class BasketAdmin(admin.ModelAdmin):
+    list_display = ["id", "uIDX"]
