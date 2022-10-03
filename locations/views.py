ad99434157795eca01640a4658bec196aac2e0c2
paka_coding <wnstjd5408@naver.com>
2022-10-03 14:56:55 +0900

    refactor: 페이지 보이는 갯수 수정

--- locations/views.py
+++ locations/views.py
@@ -27,7 +27,7 @@ class ApartmentSearchList(ListView):
     model = Apartment
     context_object_name = "aptlist"
     template_name: str = "location/aptSearchList.html"
-    paginate_by = 20
+    paginate_by = 40
 
     def get_queryset(self):
         search_keyword = self.request.GET.get("search", "")
@@ -64,7 +64,7 @@ class LocationDetailView(MultipleObjectMixin, DetailView):
     context_object_name = "aptlist"
 
     model = Location
-    paginate_by = 20
+    paginate_by = 40
 
     def get_context_data(self, **kwargs):
         object_list = Apartment.objects.filter(location=self.get_object())
